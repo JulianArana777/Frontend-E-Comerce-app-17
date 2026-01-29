d4f0f18d1f65dcac754d6d54b5be8b7fc4946619
JulianArana777 <julianaaranag@gmail.com>
2026-01-29 15:21:54 -0500

    Added bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ModalModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
