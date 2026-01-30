15b563cdf568cb528d76712decf7ded085dd4b4f
JulianArana777 <julianaaranag@gmail.com>
2026-01-30 10:59:58 -0500

    Added http Module and retrieve product from back

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ModalModule, NavBarComponent],
+  imports: [RouterOutlet, ModalModule, NavBarComponent, HttpClientModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Client';
+  products: any[] = [];
+  constructor(private http:HttpClient){}
+  ngOnInit(): void {
+    this.http.get("http://localhost:5079/api/product?PageSize=50").subscribe( (response: any) => {
+      this.products=response.data;
+    }, error => {
+      console.log(error);
+    } );    
+  }
+  
 }
