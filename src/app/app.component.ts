ff29171a68b97e1e4455035100798b386285fc52
JulianArana777 <julianaaranag@gmail.com>
2026-01-29 22:25:15 -0500

    Added a nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ModalModule],
+  imports: [RouterOutlet, ModalModule, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
